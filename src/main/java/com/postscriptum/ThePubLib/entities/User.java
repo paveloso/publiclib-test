77554ce805227be41660642821ab384e50ae614c
paveloso <saikinspav@gmail.com>
2018-11-17 23:08:50 +0300

    user toString

--- src/main/java/com/postscriptum/ThePubLib/entities/User.java
+++ src/main/java/com/postscriptum/ThePubLib/entities/User.java
@@ -29,6 +29,11 @@ public class User implements UserDetails {
     private Set<Book> books;
 
     @Override
+    public String toString() {
+        return username;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
